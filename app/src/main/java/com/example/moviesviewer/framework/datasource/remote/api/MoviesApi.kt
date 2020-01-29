78cce4fbc573dd7e2264a63a5dffbfa85c9e6a85
AlexSyt <asytnyk0@gmail.com>
2020-01-29 17:52:00 +0200

    Fix API path

--- app/src/main/java/com/example/moviesviewer/framework/datasource/remote/api/MoviesApi.kt
+++ app/src/main/java/com/example/moviesviewer/framework/datasource/remote/api/MoviesApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface MoviesApi {
 
-    @GET("/discover/movie")
+    @GET("discover/movie")
     suspend fun getMovies(
         @Query("api_key") apiKey: String,
         @Query("primary_release_date.gte") releaseDateGte: String,
