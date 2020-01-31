eac9fbb66143bacdd9b8f351a30acc1efd186b2e
AlexSyt <asytnyk0@gmail.com>
2020-01-31 12:49:41 +0200

    Fix MainActivity formatting

--- app/src/main/java/com/example/moviesviewer/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviesviewer/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.moviesviewer.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.moviesviewer.R
 
 class MainActivity : AppCompatActivity() {
