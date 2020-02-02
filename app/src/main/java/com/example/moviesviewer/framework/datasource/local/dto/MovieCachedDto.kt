0076b69788e7ca9315208545bfd3a46f9d6ebe61
AlexSyt <asytnyk0@gmail.com>
2020-02-02 14:56:16 +0200

    Create MovieCachedDto

---
+++ app/src/main/java/com/example/moviesviewer/framework/datasource/local/dto/MovieCachedDto.kt
@@ -0,0 +1,13 @@
+package com.example.moviesviewer.framework.datasource.local.dto
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class MovieCachedDto(
+    @PrimaryKey @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "poster_path") val posterPath: String?
+)
