920bab52352fc23de586a5bccf5bae8427d82e05
AlexSyt <asytnyk0@gmail.com>
2020-02-02 14:58:30 +0200

    Provide dependencies

--- app/src/main/java/com/example/moviesviewer/framework/AppModule.kt
+++ app/src/main/java/com/example/moviesviewer/framework/AppModule.kt
@@ -1,17 +1,26 @@
 package com.example.moviesviewer.framework
 
+import android.content.Context
+import androidx.room.Room
 import com.example.core.data.MoviesDataSource
 import com.example.core.domain.interactor.GetMoviesUseCase
 import com.example.core.domain.repository.MoviesRepository
 import com.example.core.domain.repository.MoviesRepositoryImpl
 import com.example.moviesviewer.BuildConfig
+import com.example.moviesviewer.framework.datasource.local.MoviesLocalDataSource
+import com.example.moviesviewer.framework.datasource.local.db.MoviesDatabase
 import com.example.moviesviewer.framework.datasource.remote.MoviesRemoteDataSource
 import com.example.moviesviewer.framework.datasource.remote.api.MoviesApi
 import com.example.moviesviewer.presentation.MoviesViewModel
 import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.Qualifier
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+val remote: Qualifier = named("remote")
+val local: Qualifier = named("local")
 
 val appModule = module {
 
@@ -20,11 +29,16 @@ val appModule = module {
     single { provideMoviesApi(get()) }
     single { provideApiKey() }
 
+    // Database
+    single { provideMoviesDatabase(get()) }
+    single { (get() as MoviesDatabase).moviesDao() }
+
     // DataSource
-    single<MoviesDataSource> { MoviesRemoteDataSource(get(), get()) }
+    single<MoviesDataSource>(remote) { MoviesRemoteDataSource(get(), get()) }
+    single<MoviesDataSource>(local) { MoviesLocalDataSource(get()) }
 
     // Repository
-    single<MoviesRepository> { MoviesRepositoryImpl(get()) }
+    single<MoviesRepository> { MoviesRepositoryImpl(get(remote), get(local)) }
 
     // Interactor
     single { GetMoviesUseCase(get()) }
@@ -42,3 +56,8 @@ fun provideRetrofit(): Retrofit =
 fun provideMoviesApi(retrofit: Retrofit): MoviesApi = retrofit.create(MoviesApi::class.java)
 
 fun provideApiKey(): String = BuildConfig.API_KEY
+
+fun provideMoviesDatabase(context: Context): MoviesDatabase =
+    Room.databaseBuilder(context.applicationContext, MoviesDatabase::class.java, "movies.db")
+        .fallbackToDestructiveMigration()
+        .build()
