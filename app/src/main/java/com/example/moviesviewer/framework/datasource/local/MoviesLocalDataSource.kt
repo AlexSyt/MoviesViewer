2b33882ddd28260f5e5e44d649c731029277f638
AlexSyt <asytnyk0@gmail.com>
2020-02-02 14:58:07 +0200

    Implement a local data source

---
+++ app/src/main/java/com/example/moviesviewer/framework/datasource/local/MoviesLocalDataSource.kt
@@ -0,0 +1,33 @@
+package com.example.moviesviewer.framework.datasource.local
+
+import com.example.core.common.Mapper
+import com.example.core.common.Result
+import com.example.core.data.MoviesDataSource
+import com.example.core.domain.model.Movie
+import com.example.moviesviewer.framework.datasource.local.db.MoviesDao
+import com.example.moviesviewer.framework.datasource.local.dto.MovieCachedDto
+import com.example.moviesviewer.framework.datasource.local.mapper.CachedMovieToMovieMapper
+import com.example.moviesviewer.framework.datasource.local.mapper.MovieToCachedMovieMapper
+
+class MoviesLocalDataSource(
+    private val moviesDao: MoviesDao
+) : MoviesDataSource {
+
+    private val cachedMovieToMovie: Mapper<MovieCachedDto, Movie> = CachedMovieToMovieMapper()
+    private val movieToCachedMovie: Mapper<Movie, MovieCachedDto> = MovieToCachedMovieMapper()
+
+    override suspend fun getMovies(
+        releaseDateGte: String,
+        releaseDateLte: String
+    ): Result<List<Movie>> =
+        try {
+            Result.Success(moviesDao.getMovies().map(cachedMovieToMovie::map))
+        } catch (exception: Exception) {
+            Result.Error(exception)
+        }
+
+    override suspend fun saveMovie(movie: Movie) =
+        moviesDao.insertMovie(movieToCachedMovie.map(movie))
+
+    override suspend fun removeAllMovies() = moviesDao.deleteMovies()
+}
